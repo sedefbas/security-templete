a0a6948f49f1e68bd1b5d3e539b41d7165cc0c88
sedef bas <bassedef98@gmail.com>
2024-08-19 13:36:14 +0300

    Initial commit

---
+++ src/test/java/com/example/spring_security_templete/SpringSecurityTempleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_templete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTempleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
